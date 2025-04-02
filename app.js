133181eacb60498b93d94eee700230c08413f071
mimimgg <com42123@gmail.com>
2025-04-02 14:43:36 +0900

    text: addEventListener

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
-// if를 사용해서 음주가 가능한 나이를 확인함
+const h1 = document.querySelector(".hello h1");
 
-// prompt - 사용자가 입력할 수 있는 창을 만듦
-//
-const age = parseInt(prompt("How old are u?"));
-
-if (isNaN(age) || age < 0) {
-  // condition === true
-  console.log("please write a real positive number");
-} else if (age < 18) {
-  console.log("your too young");
-} else if (age >= 18 && age <= 50) {
-  console.log("you can drink");
-} else if (age > 50 && age <= 80) {
-  console.log("you should exercise");
-} else if (age > 80) {
-  console.log("you can do whatever you want");
-} else {
-  console.log("you can't drink");
+function handletTitleClick() {
+  h1.style.color = "blue";
 }
+
+function handleMouseEnter() {
+  h1.innerText = "mouse is here";
+}
+function handleMouseLeave() {
+  h1.innerText = "mouse is gone";
+}
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+function handleWindowCopy() {
+  alert("copier");
+}
+function handleWindowOffline() {
+  alert("sos no wifi");
+}
+function handleWindowOnline() {
+  alert("all good");
+}
+
+// h1.onclick = handleh1Click;
+h1.addEventListener("click", handletTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
