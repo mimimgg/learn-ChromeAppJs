999d86965c1c44ae3168c53c657bbf70a2bda237
mimimgg <com42123@gmail.com>
2025-04-03 20:56:38 +0900

    text: nico

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-// 유효성검사
-// 입력된 데이터가 정해진 규칙에 맞는지 확인하는 검증 작업
-// 예) 값이 있어야만 출력 가능 / 값이 없으면 출력 불가 ... 등의 조건
+const link = document.querySelector("a");
 
-// 아래 코드의 유효성 검사
-// 1) 값이 없으면 please write your name
-// 2) 15글자가 넘으면 your name is too long
-
-// string의 길이를 구하는 방법
-// 변수명.length >= 15
-
-function handleLoginClick() {
-  const username = loginInput.value;
-  if(username === "") {
-    alert("please write your name");
-  } else if (username.length >= 15) {
-    alert("your name is too long")
-  } else {
-    
-  }
+function onLoginSubmit(e) {
+  e.preventDefault();
+  // const username = loginInput.value;
+  console.log(e);
 }
 
-loginButton.addEventListener("click", handleLoginClick);
+function handleLink(e) {
+  e.preventDefault();
+  console.log("click");
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLink);
