94d35a3980ac5238779d0549f11fc621c36d40f1
mimimgg <com42123@gmail.com>
2025-04-03 21:10:37 +0900

    -
    1. 버튼 클릭 시 hidden해서 form을 숨김
    2. 유저의 이름을 변수로 저장하고 값을 h1안에 넣음
    3. input은 사라지고 작성한 h1값이 노출된다.

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+// string만 포함된 변수는 대문자로 표기한다.
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(e) {
   e.preventDefault();
-  // const username = loginInput.value;
-  console.log(e);
-}
 
-function handleLink(e) {
-  e.preventDefault();
-  console.log("click");
+  // hidden이라는 클래스를 더해줘서 form을 숨긴다
+  loginForm.classList.add('hidden');
+
+  // 유저의 이름을 변수로 저장하고 그 이름은 h1안에 넣는다
+  const username = loginInput.value;
+
+  greeting.innerText = "Hello " + username;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLink);
