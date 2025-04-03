2e4992c6f0babfdae0fda43f7f3a19eb491c5db5
mimimgg <com42123@gmail.com>
2025-04-03 22:04:16 +0900

    localStorage
    setItem : 로컬스토리지 세팅
    getItem : 로컬스토리지 불러오기
    removeItem : 로컬스토리지 지우기

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ function onLoginSubmit(e) {
   e.preventDefault();
 
   // hidden이라는 클래스를 더해줘서 form을 숨긴다
-  loginForm.classList.add('hidden');
+  loginForm.classList.add(HIDDEN_CLASSNAME);
 
   // 유저의 이름을 변수로 저장하고 그 이름은 h1안에 넣는다
   const username = loginInput.value;
 
-  greeting.innerText = "Hello " + username;
+  // 로컬스토리지
+  localStorage.setItem("username", username);
+
+  greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
