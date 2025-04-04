19ad1855628e9410d2a2136e890ab8183f12f3a6
mimimgg <com42123@gmail.com>
2025-04-04 16:28:08 +0900

    clock 구현
    - padStart(2, "0")
    두자리 수가 나오게 하고, 한자리 수만 있으면 앞에 0을 붙인다

--- js/clock.js
+++ js/clock.js
@@ -4,9 +4,15 @@ const clock = document.querySelector("h2#clock");
 // 매번 일어나야하는 무언가
 // 예를 들면 2초마다 무슨 일이 일어나게 할 때
 
-function sayHello() {
-  console.log("hello");
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
+getClock();
+
 // setInterval (호출하려는 펑션 이름, 호출 시간)
-setInterval(sayHello, 5000);
+setInterval(getClock, 1000);
