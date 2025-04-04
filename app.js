e900e5d6dba8ac8376cad00912e1e20ed14c3ead
mimimgg <com42123@gmail.com>
2025-04-04 12:05:55 +0900

    localStorage 사용하여 사용자 정보 불러오기 총정리
    step별로 요구사항을 정의하여 작성하면 좀 더 수월하다.

--- app.js
+++ app.js
@@ -2,37 +2,40 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
-// string만 포함된 변수는 대문자로 표기한다.
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
-function onLoginSubmit(e) {
-  e.preventDefault();
-
-  // hidden이라는 클래스를 더해줘서 form을 숨긴다
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-
-  // 유저의 이름을 변수로 저장하고 그 이름은 h1안에 넣는다
-  const username = loginInput.value;
-
-  // 로컬스토리지
-  localStorage.setItem(USERNAME_KEY, username);
-
-  paintGreetings(username);
+function onLoginSubmit(event) {
+event.preventDefault();
+// step1. 화면 새로고침 방지
+loginForm.classList.add(HIDDEN_CLASSNAME);
+// step2. form을 다시 숨겨준다
+const username = loginInput.value;
+// step3. value를 username이라는 key값으로 저장시켜준다
+localStorage.setItem(USERNAME_KEY, username);
+// step4. username값을 username이라는 key와 함께 local storeage에 저장한다
+paintGreetings(username);
+// onLoginSubmit함수에서는 유저정보가 input으로부터 오고 있다
 }
 
-function paintGreetings(username){
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+function paintGreetings(username) {
+greeting.innerText = `Hello ${username}`;
+greeting.classList.remove(HIDDEN_CLASSNAME);
 }
+// step5. 비어있는 h1 요소안에 `Hello username` 이라는 텍스트 추가
 
-const saveUsername = localStorage.getItem(USERNAME_KEY);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-if(saveUsername === null) {
-  // show the form
-  loginForm.classList.remove(HIDDEN_CLASSNAME);
-  loginForm.addEventListener("submit", onLoginSubmit);
+// step6. 앱이 시작되면 local storage에서 savedusername을 얻으려고 할텐데 거기서 username이라는 key를 가지고 찾게 된다
+
+// step7. 처음에는 key랑 value가 null이므로
+if (savedUsername === null) {
+loginForm.classList.remove(HIDDEN_CLASSNAME);
+// form에 hidden을 지워주고
+loginForm.addEventListener("submit", onLoginSubmit);
+// form이 submit될때만 onLoginSubmit함수를 실행 시키도록 한다
 } else {
-  // show the greetings
-  paintGreetings(saveUsername);
+paintGreetings(savedUsername);
+// 유저정보가 localstoreage에서 나오고 있다
+// paintGreeting은 only localstoarage에서만 값을 불러온다
 }
