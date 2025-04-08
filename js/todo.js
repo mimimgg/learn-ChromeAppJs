774e3a5ced5509baba81cdc3a3328575d3f95085
mimimgg <com42123@gmail.com>
2025-04-09 01:31:41 +0900

    Update todo.js
    - X버튼을 눌러서 삭제하면 로컬스토리지에서도 삭제한다.
    - id: Date.now() 를 사용한 이유
    // 같은 값을 두개 이상 입력했을 경우 특정 id로 구분하기 위함

--- js/todo.js
+++ js/todo.js
@@ -12,13 +12,15 @@ function saveToDos() {
 
 function deleteToDo(event) {
   const li = event.target.parentElement;
+  console.log(li.id);
   li.remove();
 }
 
 function paintToDo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
 
   const button = document.createElement("button");
   button.innerText = "❌";
@@ -33,8 +35,12 @@ function handleToDoSumit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  toDos.push(newTodo);
-  paintToDo(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
   saveToDos();
 }
 
@@ -47,6 +53,5 @@ if (savedToDos !== null) {
   toDos = parsedToDos;
   parsedToDos.forEach(paintToDo);
 }
-
 
 
