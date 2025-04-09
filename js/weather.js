fc0d071dd30faecab6da4b45d819f9da6902897a
mimimgg <com42123@gmail.com>
2025-04-09 18:34:55 +0900

    fix: 요구사항정의

--- js/weather.js
+++ js/weather.js
@@ -9,14 +9,14 @@ const API_KEY = "ab76ac83f06f5acf5f1dcc01738bcf37";
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=mretric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       const weather = document.querySelector("#weather span:first-child");
       const city = document.querySelector("#weather span:last-child");
       city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
     });
 }
 function onGeoError() {
