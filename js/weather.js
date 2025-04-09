c0b0508be8b96f9a20619be6009a315bd1adad67
mimimgg <com42123@gmail.com>
2025-04-09 17:52:54 +0900

    weather

--- js/weather.js
+++ js/weather.js
@@ -4,14 +4,23 @@
 // 1) 잘 되었을 때 실행될 함수
 // 2) 에러 발생 시 실행될 함수
 
+const API_KEY = "ab76ac83f06f5acf5f1dcc01738bcf37";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("You live in", lat, lng);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=mretric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 function onGeoError() {
   alert("Can't find you. No weather for you");
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
-
