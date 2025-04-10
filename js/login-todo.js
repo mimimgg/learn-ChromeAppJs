2ea1c06055fa462d406e310782a216ebcb18408b
mimimgg <com42123@gmail.com>
2025-04-11 01:07:27 +0900

    localStorage.getItem

--- js/login-todo.js
+++ js/login-todo.js
@@ -3,7 +3,8 @@ const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector(".greeting");
 
 // string만 포함된 변수는 대문자로 담는다
-const HIDDEN_CLASSNAME = "hidden"
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function handleLoginClick(e) {
   e.preventDefault();
@@ -11,10 +12,26 @@ function handleLoginClick(e) {
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   // 로컬스토리지에 이름을 담는다.
-  localStorage.setItem("username", username);
-  greeting.innerText = `HELLO ${username}👋`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
-  
+  localStorage.setItem(USERNAME_KEY, username);
+  // 환영인사 남기기
+  paintGreetings(username);
 }
 
 loginForm.addEventListener("submit", handleLoginClick);
+
+function paintGreetings(username){
+  greeting.innerText = `HELLO ${username}👋`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+// localstorage에 입력된 값 가져오기
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if (savedUserName === null) {
+  // 입력 form 노출
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", handleLoginClick);
+} else {
+  // greeting 노출
+  paintGreetings(savedUserName);
+}
